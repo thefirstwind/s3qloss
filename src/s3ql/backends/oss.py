db499609e1caba70de9335b3b882787e805a74e9
xingxn <thefirstwind@gmail.com>
2013-09-20 23:04:00 +0800

    sss

--- src/s3ql/backends/oss.py
+++ src/s3ql/backends/oss.py
@@ -651,37 +651,35 @@ class ObjectW(object):
 #     @retry
     def close(self):
         pass
-#         '''Close object and upload data'''
-# 
-#         # Access to protected member ok
-#         #pylint: disable=W0212
-# 
-#         log.debug('ObjectW(%s).close(): start', self.key)
-# 
-#         self.closed = True
-#         self.headers['Content-Length'] = self.obj_size
-# 
-#         self.fh.seek(0)
-#         resp = self.backend._do_request('PUT', '/%s%s' % (self.backend.prefix, self.key),
-#                                        headers=self.headers, body=self.fh)
-
-#        etag = ""
+        '''Close object and upload data'''
+ 
+        # Access to protected member ok
+        #pylint: disable=W0212
+ 
+        log.debug('ObjectW(%s).close(): start', self.key)
+ 
+        self.closed = True
+        self.headers['Content-Length'] = self.obj_size
+ 
+        self.fh.seek(0)
+        resp = self._do_request('PUT', '/%s%s' % (self.backend.prefix, self.key),
+                                       headers=self.headers, body=self.fh)
 ###kei------
-#         etag = resp.getheader('ETag').strip('"')
-#         if resp is not None:
-#             etag = resp.getheader('ETag').strip('"')
-#             assert resp.length == 0
-# 
-#         if etag != self.md5.hexdigest():
-#             log.warn('ObjectW(%s).close(): MD5 mismatch (%s vs %s)', self.key, etag,
-#                      self.md5.hexdigest)
-#             try:
-#                 self.backend.delete(self.key)
-#             except:
-#                 log.exception('Objectw(%s).close(): unable to delete corrupted object!',
-#                               self.key)
+        etag = resp.getheader('ETag').strip('"')
+        if resp is not None:
+            etag = resp.getheader('ETag').strip('"')
+            assert resp.length == 0
+ 
+        if etag != self.md5.hexdigest():
+            log.warn('ObjectW(%s).close(): MD5 mismatch (%s vs %s)', self.key, etag,
+                  self.md5.hexdigest)
+            try:
+                self.backend.delete(self.key)
+            except:
+                log.exception('Objectw(%s).close(): unable to delete corrupted object!',
+                               self.key)
 #kei----------
-#             raise BadDigestError('BadDigest', 'Received ETag does not agree with our calculations.')
+            raise BadDigestError('BadDigest', 'Received ETag does not agree with our calculations.')
 
     def __enter__(self):
         return self
