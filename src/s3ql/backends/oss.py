68a958142328b790e25b0825d0504f2b9f37653c
xingxn <thefirstwind@gmail.com>
2013-09-22 00:57:59 +0800

    s

--- src/s3ql/backends/oss.py
+++ src/s3ql/backends/oss.py
@@ -457,7 +457,7 @@ class Backend(s3c.Backend):
         # Always include bucket name in path for signing
         print("xxxxxxx path xxxx: %s" % path)
 #         sign_path = urllib.quote('/%s%s' % (self.bucket_name, path))
-        sign_path = ''.append('/%s/%s' % (self.bucket_name, path))
+        sign_path = '/%s/%s' % (self.bucket_name, path)
         print("xxxxxxx sign_path xxxx: %s" % sign_path)
         auth_strs.append(sign_path)
         if subres:
